9aeeaf527b0a5930ee8294ad6af4f7ecd3369474
Claudine <claudine.bananal@gmail.com>
2016-08-20 15:34:52 -0700

    structure and style sticky header on mobile view

--- themes/givingbackissexy/header.php
+++ themes/givingbackissexy/header.php
@@ -26,6 +26,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+			<div><img class="hamburger-menu" src="<?php echo get_template_directory_uri() ?>/assets/images/icons/hamburger.png"></div>
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
