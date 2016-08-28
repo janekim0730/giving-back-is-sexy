50d80191b3d76a41058114189df20841c1addccc
Claudine <claudine.bananal@gmail.com>
2016-08-28 14:23:49 -0700

    enqueue main scripts of the theme

--- themes/givingbackissexy/functions.php
+++ themes/givingbackissexy/functions.php
@@ -123,6 +123,8 @@ function givingbackissexy_scripts() {
 
 	wp_enqueue_script( 'givingbackissexy-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_script( 'givingbackissexy', get_template_directory_uri() . '/build/js/main.min.js', array(), false, true );
+
 	wp_enqueue_script( 'smoothup', get_template_directory_uri() . '/js/smoothscroll.js', array( 'jquery' ), '',  true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
