785025c58b166b60fc1c56b1bfbac18b48e28ded
Claudine <claudine.bananal@gmail.com>
2016-08-28 18:47:06 -0700

    draft structure and styling of contact page

--- themes/givingbackissexy/contact-us.php
+++ themes/givingbackissexy/contact-us.php
@@ -1,6 +1,11 @@
 <?php
 /**
- * The template for displaying contact page.
+ * The template for displaying Contact Us page.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -13,18 +18,55 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main container" role="main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+			<?php while ( have_posts() ) : the_post(); ?>
 
-				get_template_part( 'template-parts/content', 'page' );
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
+					<header class="entry-header container">
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					</header><!-- .entry-header -->
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
+					<div class="entry-content container">
+						<div class="contact-container">
+						  <img class="contact-icon" src="<?php echo CFS()->get( 'mail_icon' ); ?>"/>
+							<?php echo CFS()->get( 'e_mail' ); ?>
+						</div>
+						<div class="contact-container">
+							<img class="contact-icon" src="<?php echo CFS()->get( 'phone_icon' ); ?>"/>
+							<a href=""><?php echo CFS()->get( 'phone_number' ); ?></a>
+						</div>
+						<?php
+							the_content();
+
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'givingbackissexy' ),
+								'after'  => '</div>',
+							) );
+						?>
+					</div><!-- .entry-content -->
+
+					<?php if ( get_edit_post_link() ) : ?>
+						<footer class="entry-footer">
+							<?php
+								edit_post_link(
+									sprintf(
+										/* translators: %s: Name of current post */
+										esc_html__( 'Edit %s', 'givingbackissexy' ),
+										the_title( '<span class="screen-reader-text">"', '"</span>', false )
+									),
+									'<span class="edit-link">',
+									'</span>'
+								);
+							?>
+						</footer><!-- .entry-footer -->
+					<?php endif; ?>
+				</article><!-- #post-## -->
+
+				<!-- If comments are open or we have at least one comment, load up the comment template. -->
+				<?php if ( comments_open() || get_comments_number() ) :
 					comments_template();
-				endif;
+				endif; ?>
 
-			endwhile; // End of the loop.
-			?>
+			<?php endwhile; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
