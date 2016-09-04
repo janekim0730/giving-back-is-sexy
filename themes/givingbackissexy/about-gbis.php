ca9da4c0056e69557abc334804c8b0b6bacf5cfa
janekim0730 <janekim0730@gmail.com>
2016-09-04 05:13:59 -0700

    styling about gbis page, need media query

--- themes/givingbackissexy/about-gbis.php
+++ themes/givingbackissexy/about-gbis.php
@@ -1,16 +1,10 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * Template Name: About GBIS
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package Giving_Back_Is_Sexy
- * Template Name: About GBIS
  */
 
 get_header(); ?>
@@ -18,77 +12,38 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main container" role="main">
 
-			<img src="<?php echo get_template_directory_uri()?>/assets/images/logos/logo.png" alt="sexy">
+			<div class="hashtag-wrapper"><img class="gbis-hashtag" src="<?php echo get_template_directory_uri()?>/assets/images/logos/logo.png" alt="giving back is sexy hashtag"></div>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
-					<header class="entry-header">
-						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-					</header><!-- .entry-header -->
+					<div class="about-gbis-content">
 
-					<div class="entry-content">
 						<div class="about-gbis-container">
-							<div class="about-gbis-icon">
-								<img src="<?php echo CFS()->get ( 'icon_1' ); ?>">
-							</div>
+								<img class="about-gbis-icon" src="<?php echo CFS()->get ( 'icon_1' ); ?>">
 							<?php echo CFS()->get ( 'description_1' ); ?>
 						</div>
+
 						<div class="about-gbis-container">
-							<div class="about-gbis-icon">
-								<img src="<?php echo CFS()->get ( 'icon_2' ); ?>">
-							</div>
+								<img class="about-gbis-icon" src="<?php echo CFS()->get ( 'icon_2' ); ?>">
 							<?php echo CFS()->get ( 'description_2' ); ?>
 						</div>
+
 						<div class="about-gbis-container">
-							<div class="about-gbis-icon">
-								<img src="<?php echo CFS()->get ( 'icon_3' ); ?>">
-							</div>
+								<img class="about-gbis-icon" src="<?php echo CFS()->get ( 'icon_3' ); ?>">
 							<?php echo CFS()->get ( 'description_3' ); ?>
 						</div>
+
 						<div class="about-gbis-container">
-							<div class="about-gbis-icon">
-								<img src="<?php echo CFS()->get ( 'icon_4' ); ?>">
-							</div>
+								<img class="about-gbis-icon" src="<?php echo CFS()->get ( 'icon_4' ); ?>">
 							<?php echo CFS()->get ( 'description_4' ); ?>
 						</div>
+
 						<div class="about-gbis-container">
-							<div class="about-gbis-icon">
-								<img src="<?php echo CFS()->get ( 'icon_5' ); ?>">
-							</div>
+								<img class="about-gbis-icon" src="<?php echo CFS()->get ( 'icon_5' ); ?>">
 							<?php echo CFS()->get ( 'description_5' ); ?>
 						</div>
-						<?php
-							// the_content();
-							//
-							// wp_link_pages( array(
-							// 	'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'givingbackissexy' ),
-							// 	'after'  => '</div>',
-							// ) );
-						?>
 					</div><!-- .entry-content -->
 
-					<?php if ( get_edit_post_link() ) : ?>
-						<footer class="entry-footer">
-							<?php
-								edit_post_link(
-									sprintf(
-										/* translators: %s: Name of current post */
-										esc_html__( 'Edit %s', 'givingbackissexy' ),
-										the_title( '<span class="screen-reader-text">"', '"</span>', false )
-									),
-									'<span class="edit-link">',
-									'</span>'
-								);
-							?>
-						</footer><!-- .entry-footer -->
-					<?php endif; ?>
-				</article><!-- #post-## -->
-
-				<!-- If comments are open or we have at least one comment, load up the comment template. -->
-				<?php if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif; ?>
 
 			<?php endwhile; ?>
 
