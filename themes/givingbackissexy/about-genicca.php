6c7fa1144c0d35d58984bc3e9d00d3e08b5e737a
janekim0730 <janekim0730@gmail.com>
2016-09-04 04:27:29 -0700

    styling on about genicca global group

--- themes/givingbackissexy/about-genicca.php
+++ themes/givingbackissexy/about-genicca.php
@@ -1,16 +1,12 @@
 <?php
 /**
- * The template for displaying all pages.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * Template Name: About Genicca
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package Giving_Back_Is_Sexy
- * Template Name: About Genicca
+ *
  */
 
 get_header(); ?>
@@ -20,11 +16,12 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
-					<div>
-						<a href="http://www.genicca.com" target="_blank"><img src="<?php echo CFS()->get ( 'logo_image' ); ?>"></a>
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+						<div class="logo-wrapper"><a href="http://www.genicca.com" target="_blank"><img src="<?php echo CFS()->get ( 'logo_image' ); ?>"></a>
 						<img class="profile-photo" src="<?php echo CFS()->get ( 'profile_photo' ); ?>">
-					</div>
+					  </div>
+
 					<header class="entry-header container">
 						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 					</header><!-- .entry-header -->
@@ -40,28 +37,7 @@ get_header(); ?>
 						?>
 					</div><!-- .entry-content -->
 
-					<?php if ( get_edit_post_link() ) : ?>
-						<footer class="entry-footer">
-							<?php
-								edit_post_link(
-									sprintf(
-										/* translators: %s: Name of current post */
-										esc_html__( 'Edit %s', 'givingbackissexy' ),
-										the_title( '<span class="screen-reader-text">"', '"</span>', false )
-									),
-									'<span class="edit-link">',
-									'</span>'
-								);
-							?>
-						</footer><!-- .entry-footer -->
-					<?php endif; ?>
-				</article><!-- #post-## -->
-
-				<!-- If comments are open or we have at least one comment, load up the comment template. -->
-				<?php if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif; ?>
-
+				</div><!-- #post-## -->
 			<?php endwhile; ?>
 
 		</main><!-- #main -->
