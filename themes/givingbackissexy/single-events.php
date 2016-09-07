da2b56178843fb7821146cc4fe54d327a599f309
janekim0730 <janekim0730@gmail.com>
2016-09-07 00:15:18 -0700

    add WP_Flickity plugin for sponsors and vendors section

--- themes/givingbackissexy/single-events.php
+++ themes/givingbackissexy/single-events.php
@@ -12,13 +12,21 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+<?php while ( have_posts() ) : the_post(); ?>
 
-<?php the_post_thumbnail('large'); ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="event-header">
+					<?php if ( has_post_thumbnail() ) : ?>
+					<?php the_post_thumbnail( 'large' ); ?>
+					<?php endif; ?>
+				</header><!---header-->
+
+				<div class="event-content">
+							<?php the_content(); ?>
+				</div><!---content-->
 
 
-<div><?php echo CFS()->get( 'event_description' ); ?></div>
-
-<div><?php
+<?php
 			$dates = CFS() -> get ('event_date');
 			foreach ($dates as $date) :
 			?>
@@ -29,6 +37,33 @@ get_header(); ?>
 
 <?php endforeach; ?></div>
 
+ <div class="event-description"><?php echo CFS()->get( 'event_description' ); ?></div>
+
+ <h1>Featured Sponsors</h1>
+		 <h2 class="platinum-sponsor">Platinum</h2>
+			<?php echo do_shortcode("[wp_flickity id='1']"); ?>
+
+		<h2 class="gold-sponsor">Gold</h2>
+		<?php echo do_shortcode("[wp_flickity id='2']"); ?>
+
+		<h2 class="silver-sponsor">Silver</h2>
+		<?php echo do_shortcode("[wp_flickity id='5']"); ?>
+
+		<h2 class="bronze-sponsor">Bronze</h2>
+		<?php echo do_shortcode("[wp_flickity id='6']"); ?>
+
+ <h1>Contributing Sponsors</h1>
+  	<?php echo do_shortcode("[wp_flickity id='7']"); ?>
+
+<h1>Vendors</h1>
+ 	<?php echo do_shortcode("[wp_flickity id='8']"); ?>
+
+
+
+
+
+</article>
+<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
