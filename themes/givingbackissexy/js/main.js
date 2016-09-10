c9d02d5a961935489cef32626bf2698e4f659a1e
Claudine <claudine.bananal@gmail.com>
2016-09-10 00:43:07 -0700

    draft hovering of sub-menu items

--- themes/givingbackissexy/js/main.js
+++ themes/givingbackissexy/js/main.js
@@ -1,3 +1,16 @@
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+jQuery(document).ready(function() {
+
+  //Toggle sub-menu and space it properly under its parent
+  jQuery('.main-navigation .menu-item-has-children').on('hover', function() {
+
+    jQuery(this).children('.sub-menu').toggle(function() {
+      jQuery('.main-navigation .sub-menu').css({position:"relative", top:"0", left:"0"});
+    });
+
+  });
+
+}); //end document ready
