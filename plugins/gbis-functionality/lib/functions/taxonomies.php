e34977a94d61f40f540f71a3289b6ce7c44796b2
Claudine <claudine.bananal@gmail.com>
2016-09-11 18:13:53 -0700

    fix custom functionality for post types and taxonomies

--- plugins/gbis-functionality/lib/functions/taxonomies.php
+++ plugins/gbis-functionality/lib/functions/taxonomies.php
@@ -6,40 +6,41 @@
  */
 
  // Register Custom Taxonomy
- function register_event_type_tax() {
+ // Register Custom Taxonomy
+function gbis_register_events_tax() {
 
- 	$labels = array(
- 		'name'                       => 'Event Types',
- 		'singular_name'              => 'Event Type',
- 		'menu_name'                  => 'Product Type',
- 		'all_items'                  => 'All Event Types',
- 		'parent_item'                => 'Parent Event Type',
- 		'parent_item_colon'          => 'Parent Event Type:',
- 		'new_item_name'              => 'New Event Type Name',
- 		'add_new_item'               => 'Add New Event Type',
- 		'edit_item'                  => 'Edit Event Type',
- 		'update_item'                => 'Update Event Type',
- 		'view_item'                  => 'View Event Type',
- 		'separate_items_with_commas' => 'Separate event types with commas',
- 		'add_or_remove_items'        => 'Add or remove event types',
- 		'choose_from_most_used'      => 'Choose from the most used',
- 		'popular_items'              => 'Popular Event Types',
- 		'search_items'               => 'Search Event Types',
- 		'not_found'                  => 'Not Found',
- 		'no_terms'                   => 'No event types',
- 		'items_list'                 => 'Event Types list',
- 		'items_list_navigation'      => 'Event Types list navigation',
- 	);
- 	$args = array(
- 		'labels'                     => $labels,
- 		'hierarchical'               => true,
- 		'public'                     => true,
- 		'show_ui'                    => true,
- 		'show_admin_column'          => true,
- 		'show_in_nav_menus'          => true,
- 		'show_tagcloud'              => false,
- 	);
- 	register_taxonomy( 'event-type', array( 'events' ), $args );
+	$labels = array(
+		'name'                       => 'Event History',
+		'singular_name'              => 'Event History',
+		'menu_name'                  => 'Event History',
+		'all_items'                  => 'All Event Categories',
+		'parent_item'                => 'Event Category',
+		'parent_item_colon'          => 'Event Category:',
+		'new_item_name'              => 'New Event Category',
+		'add_new_item'               => 'Add New Event Category',
+		'edit_item'                  => 'Edit Event Category',
+		'update_item'                => 'Update Event Category',
+		'view_item'                  => 'View Event Category',
+		'separate_items_with_commas' => 'Separate event categories with commas',
+		'add_or_remove_items'        => 'Add or remove event categories',
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular Event Categories',
+		'search_items'               => 'Search Event Categories',
+		'not_found'                  => 'Not Found',
+		'no_terms'                   => 'No event categories',
+		'items_list'                 => 'Event Categories list',
+		'items_list_navigation'      => 'Event Categories list navigation',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => false,
+	);
+	register_taxonomy( 'event_history', array( 'events' ), $args );
 
- }
- add_action( 'init', 'register_event_type_tax', 0 );
+}
+add_action( 'init', 'gbis_register_events_tax', 0 );
