0e289e8e6ecbd97dd57a03f83d083776d278f65a
janekim0730 <janekim0730@gmail.com>
2016-09-20 21:53:31 -0700

    working on the archive event page

--- themes/givingbackissexy/archive-events.php
+++ themes/givingbackissexy/archive-events.php
@@ -12,19 +12,36 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<header class="page-header">
-		  <h1>Events</h1>
-			</header>
+			<?php
+			if ( have_posts() ) : ?>
 
-		<?php	$terms = get_terms('event_history');
-			echo '<div>';
-			foreach ($terms as $term) {
-			echo '<h2><a href="'.get_term_link($term).'">'.$term->name.'</a></h2>';
-			}
-			echo '</div>'; ?>
+				<header class="page-header">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="archive-description">', '</div>' );
+					?>
+				</header><!-- .page-header -->
 
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
 
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() );
 
+				endwhile;
+
+				the_posts_navigation();
+
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
