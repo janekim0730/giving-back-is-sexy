f46c6fc32748c36e8e3f1f7a6bba497cb97d97d0
janekim0730 <janekim0730@gmail.com>
2016-09-23 17:51:45 -0700

    styling footer

--- themes/givingbackissexy/footer.php
+++ themes/givingbackissexy/footer.php
@@ -21,7 +21,6 @@
 				<span class="instagram-icon"><a href="https://www.instagram.com/geniccadotcom" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></span>
 			</div>
 
-      <div class="menu-links">
 				<div class="footer-menus">
 				 <?php wp_nav_menu( array( 'menu' => 'footer' ) ); ?>
 				</div>
@@ -29,11 +28,10 @@
 				<div class="go-top">
 					<a href="#top" id="smoothup" title="Back to top"><img class="top-button" src="<?php echo get_template_directory_uri()?>/assets/images/icons/go-top-button.png" alt="go-to-top button"></a>
 				</div>
+
 		 </div>
 
 			<p class="copyright">Copyright &copy; <?php the_date('Y'); ?> Cory Manson<p>
-
-    </div>
 
 <?php wp_footer(); ?>
 	<!-- <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/build/js/footer.min.js"></script> -->
