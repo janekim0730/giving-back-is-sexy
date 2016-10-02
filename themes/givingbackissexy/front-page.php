5aef9380df8c164faf6909d96c889b3500353157
Claudine <claudine.bananal@gmail.com>
2016-10-02 14:47:45 -0700

    adjust specificity of the width of introductory text

--- themes/givingbackissexy/front-page.php
+++ themes/givingbackissexy/front-page.php
@@ -46,6 +46,7 @@ get_header(); ?>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content container">
+						<div class="intro">
 						<?php
 							the_content();
 
@@ -54,6 +55,7 @@ get_header(); ?>
 								'after'  => '</div>',
 							) );
 						?>
+						</div>
 
 						<a class="learn-more" href="<?php echo get_site_url(); ?>/givingbackissexy">Learn More</a>
 			<?php endwhile; ?>
@@ -61,88 +63,91 @@ get_header(); ?>
 		<?php endif; ?>
 
 						<!-- Upcoming Events Loop -->
-						<?php
-						/*
-						 * Loop through Categories and Display Posts within
-						 */
-						$post_type = 'events';
 
-						// Get all the taxonomies for this post type
-						$taxonomies = get_object_taxonomies( array( 'post_type' => $post_type ) );
+						<div class="event-info-container">
+							<?php
+							/*
+							 * Loop through Categories and Display Posts within
+							 */
+							$post_type = 'events';
 
-						foreach( $taxonomies as $taxonomy ) :
+							// Get all the taxonomies for this post type
+							$taxonomies = get_object_taxonomies( array( 'post_type' => $post_type ) );
 
-						    // Gets every "category" (term) in this taxonomy to get the respective posts
-						    $terms = get_terms($taxonomy );
+							foreach( $taxonomies as $taxonomy ) :
 
-						    foreach( $terms as $term ) : ?>
+							    // Gets every "category" (term) in this taxonomy to get the respective posts
+							    $terms = get_terms($taxonomy );
 
-						    <h2 class="event-tax-title"> <?php echo $term->name; ?></h2>
+							    foreach( $terms as $term ) : ?>
 
-						        <?php
-						        $args = array(
-						                'post_type' => $post_type,
-						                'posts_per_page' => -1,  //show all posts
-						                'tax_query' => array(
-						                    array(
-						                        'taxonomy' => $taxonomy,
-						                        'field' => 'slug',
-						                        'terms' => $term->slug,
+							    <h2 class="event-tax-title"> <?php echo $term->name; ?></h2>
 
-						                    )
-						                )
+							        <?php
+							        $args = array(
+							                'post_type' => $post_type,
+							                'posts_per_page' => -1,  //show all posts
+							                'tax_query' => array(
+							                    array(
+							                        'taxonomy' => $taxonomy,
+							                        'field' => 'slug',
+							                        'terms' => $term->slug,
 
-						            );
-						        $posts = new WP_Query($args);
+							                    )
+							                )
 
-						        if( $posts->have_posts() ): while( $posts->have_posts() ) : $posts->the_post(); ?>
+							            );
+							        $posts = new WP_Query($args);
 
-						                   <!-- <h2><?php  echo get_the_title(); ?></h2> -->
+							        if( $posts->have_posts() ): while( $posts->have_posts() ) : $posts->the_post(); ?>
 
-															 <div class="event-info-wrapper">
-					 											<?php
-					 														$dates = CFS() -> get ('event_date');
-					 														foreach ($dates as $date) :
-					 														?>
-					 														<div class="event-info">
-					 															<div class="event-date"><span class="event-week"><?php echo $date['event_day_of_the_week']; ?></span>
-					 															<span class="style-line"></span>
-					 															<span class="event-month"><?php echo $date['event_month']; ?></span>
-					 															<span class="event-day"><?php echo $date['event_day']; ?></span>
-					 															<span class="event-year"><?php echo $date['event_year']; ?></span>
-					 															</div>
-					 															<!--end event-date-->
-					 														</div>
-					 														<!--end event-info-->
-					 											<?php endforeach; ?>
+							                   <!-- <h2><?php  echo get_the_title(); ?></h2> -->
 
-					 			            <div class="event-info-right">
-					 										<div class="event-right-content">
-					 											 <p><?php the_title(); ?></p>
-					 											<?php
-					 														$locations = CFS() -> get ('event_location');
-					 														foreach ($locations as $location) :
-					 														?>
-					 														<div class="event-location">
-					 															<span class="event-place"><?php echo $location['event_place']; ?></span>
-					 															<span class="event-city"><?php echo $location['event_city']; ?></span>
-					 														</div>
-					 														<!--end event-location-->
-					 											<?php endforeach; ?>
-					 										</div>
-					 										<!--end event-right-content-->
-					 										<div class="external-link"><?php echo CFS()->get( 'ticket_external_link' ); ?></div>
-					 									</div>
-					 									<!--end event-info-right-->
-					 								</div>
-					 								<!--end event-info-wrapper-->
+																 <div class="event-info-wrapper">
+						 											<?php
+						 														$dates = CFS() -> get ('event_date');
+						 														foreach ($dates as $date) :
+						 														?>
+						 														<div class="event-info">
+						 															<div class="event-date"><span class="event-week"><?php echo $date['event_day_of_the_week']; ?></span>
+						 															<span class="style-line"></span>
+						 															<span class="event-month"><?php echo $date['event_month']; ?></span>
+						 															<span class="event-day"><?php echo $date['event_day']; ?></span>
+						 															<span class="event-year"><?php echo $date['event_year']; ?></span>
+						 															</div>
+						 															<!--end event-date-->
+						 														</div>
+						 														<!--end event-info-->
+						 											<?php endforeach; ?>
+
+						 			            <div class="event-info-right">
+						 										<div class="event-right-content">
+						 											 <p><?php the_title(); ?></p>
+						 											<?php
+						 														$locations = CFS() -> get ('event_location');
+						 														foreach ($locations as $location) :
+						 														?>
+						 														<div class="event-location">
+						 															<span class="event-place"><?php echo $location['event_place']; ?></span>
+						 															<span class="event-city"><?php echo $location['event_city']; ?></span>
+						 														</div>
+						 														<!--end event-location-->
+						 											<?php endforeach; ?>
+						 										</div>
+						 										<!--end event-right-content-->
+						 										<div class="external-link"><?php echo CFS()->get( 'ticket_external_link' ); ?></div>
+						 									</div>
+						 									<!--end event-info-right-->
+						 								</div>
+						 								<!--end event-info-wrapper-->
 
 
-						        <?php endwhile; endif; ?>
+							        <?php endwhile; endif; ?>
 
-						    <?php endforeach;
+							    <?php endforeach;
 
-						endforeach; ?>
+							endforeach; ?>
+						</div>
 
 						<!-- //End Upcoming Events Loop -->
 						<!-- Grabs the form from the contact page -->
